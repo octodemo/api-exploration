1fe186645cc7cb357e4f83c0947c7895a055ffb3
Kevin Alwell <alwell-kevin@github.com>
2023-10-19 17:37:09 +0000

    web service

--- webservice.js
+++ webservice.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+// create a get / endpoint that returns hello world
+
+app.get('/', (req, res) => {
+    // print request originating ip address
+
+    console.log(req.ip);
+
+    console.log(req.query);
+
+    res.send('Hello NYIT LETS GO API DEVELOPMENT!');
+});
+
+
 app.get('/user/:username', (req, res) => {
     const username = req.params.username;
     console.log(username);
