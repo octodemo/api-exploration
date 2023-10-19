e0a39cbe27816a28ccc6404874d82bece3d95524
Kevin Alwell <alwell-kevin@github.com>
2023-10-19 17:23:35 +0000

    include github service function call

--- githubService.js
+++ githubService.js
@@ -1,1 +1,21 @@
 // get github username and public information from api.github.com
+
+var request = require('request');
+var gitHubUsername = "pariima";
+
+var getProfile = function (username) {
+    var options = {
+        url: 'https://api.github.com/users/' + username,
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+           console.log("hello my friends from NYIT", response);
+        }
+    })
+}
+
+getProfile(gitHubUsername)
